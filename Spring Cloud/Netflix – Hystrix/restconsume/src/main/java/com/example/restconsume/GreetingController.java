1ef0650b79559191d039977b9d8037f1ac34fbcd
ZEUS\Ali <zafari.a@gmail.com>
2018-11-05 22:46:07 -0500

    restConsumer

---
+++ Spring Cloud/Netflix – Hystrix/restconsume/src/main/java/com/example/restconsume/GreetingController.java
@@ -0,0 +1,20 @@
+package com.example.restconsume;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Controller
+public class GreetingController {
+
+    @Autowired
+    private GreetingService greetingService;
+
+    @GetMapping ("/get-greeting/{username}")
+    public String getGreeting( Model model, @PathVariable ("username") String username) {
+        model.addAttribute("greeting", greetingService.getGreeting(username));
+        return "greeting-view";
+    }
+}
